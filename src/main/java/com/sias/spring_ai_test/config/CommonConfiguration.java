ddd5f800a41c674d42970579f1c0221d22570f4a
yicheng <2816146402@qq.com>
2026-03-30 23:36:59 +0800

    初始化项目架构

---
+++ src/main/java/com/sias/spring_ai_test/config/CommonConfiguration.java
@@ -0,0 +1,10 @@
+package com.sias.spring_ai_test.config;
+
+import org.springframework.context.annotation.Bean;
+
+public class CommonConfiguration {
+    @Bean//创建一个bean
+    public String getName(){
+        return  null;
+    }
+}
